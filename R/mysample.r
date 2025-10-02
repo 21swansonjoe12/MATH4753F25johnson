dd87721df8f74e391bfde90fe6aec3ed45b11a9b
21swansonjoe12 <21swanson.joe12@gmail.com>
2025-10-02 18:31:06 -0500

    Added function mysample()

---
+++ R/mysample.r
@@ -0,0 +1,27 @@
+#' My Sample
+#'
+#' @param n An integer
+#' @param iter An integer
+#' @param time An integer
+#'
+#' @returns A boxplot for each iteration
+#' @export
+#'
+#' @examples
+#' mysample(10000)
+mysample = function(n, iter = 10, time = 0.5){
+  for(i in 1:iter){ # assign each loop of i to be 1 through iter
+    #make a sample
+    s = sample(1:10, n, replace = TRUE) # sample of options 1 through 10, choosen n times with replacement
+    # turn the sample into a factor
+    sf = factor(s, levels = 1:10) # used for tabulation later (assigns "names" to each value to later see how many of the same value exist)
+    #make a barplot
+    barplot(table(sf)/n, beside = TRUE, col = rainbow(10), # tabulate the sample factor and divide by n to get the relative frequency
+            main = paste("Example sample()", " iteration ", i, " n= ", n,sep=""),
+            ylim = c(0,0.2)
+    )
+
+    #release the table
+    Sys.sleep(time) # delay for time seconds to prevent lag and whatnot
+  }
+}
